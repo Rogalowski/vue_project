b9e6b8f1f242e922036ce9bd888624005cd7ff47
Jacek Mateusz Rogowski <rogalowski@gmail.com>
2023-02-18 11:04:50 +0100

    changed folder project and add new course for Vue Mastery

---
+++ vue_project/src/components/MyView/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from 'vue'
+const msgr = ref("Hello World")
+</script>
+
+<template>
+  asd
+
+    <h1  >asdsd{{ msgr }}</h1>
+<input v-model="msgr">
+</template>
+
+<style>
+ h1 {
+    font-size: xx-large;
+    color: white;
+  }
+
+</style>
---
+++ vue_project/src/views/AboutView.vue
@@ -0,0 +1,47 @@
+<script  >
+
+import dataJson from "../data/data.json"
+export default ({
+    data() {
+
+    return{
+        destiantionRouter: dataJson.destinations
+      }
+}
+})
+</script>
+
+
+<template>
+   <div class="about">
+
+
+
+    <h1> About
+    <router-link
+        v-for="destination in destiantionRouter"
+        :key="destination.id"
+        :to="{name: 'destination.show', params:{id: destination.id, slug: destination.slug}} || destination.slug"
+
+        >
+        <h2>{{ destination.name}}</h2>
+        <img class="img" :src="`src/assets/images/${destination.image}`" alt="destination.name">
+   </router-link>
+  </h1>
+  </div>
+
+</template>
+
+<style scoped>
+@media  (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+
+    align-items: center;
+  }
+}
+img {
+  display: inline-block;
+  max-block-size: 200px;
+}
+</style>
