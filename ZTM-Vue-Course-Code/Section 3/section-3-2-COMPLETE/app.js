ce87616c0a63b6c8a86d33166f7e3c058748938e
Rogalowski <rogalowski@gmail.com>
2023-02-25 15:32:43 +0100

    Pass Section3 fix copy

--- ZTM-Vue-Course-Code/Section 3/section-3-2-COMPLETE/app.js
+++ ZTM-Vue-Course-Code/Section 3/section-3-2-COMPLETE/app.js
@@ -1,43 +1,48 @@
 Vue.createApp({
-  data() {
-    return {
-      perspective: 100,
-      rotateX: 0,
-      rotateY: 0,
-      rotateZ: 0
-    }
-  },
-  computed: {
-    box() {
-      return {
-        transform: `
+    data() {
+        return {
+            perspective: 100,
+            rotateX: 0,
+            rotateY: 0,
+            rotateZ: 0,
+        };
+    },
+    computed: {
+        box() {
+            return {
+                transform: `
           perspective(${this.perspective}px)
           rotateX(${this.rotateX}deg)
           rotateY(${this.rotateY}deg)
           rotateZ(${this.rotateZ}deg)
-        `
-      }
-    }
-  },
-  methods: {
-    reset() {
-      this.perspective = 100
-      this.rotateX = 0
-      this.rotateY = 0
-      this.rotateZ = 0
+        `,
+            };
+        },
     },
-    copy() {
-      const el = document.createElement('textarea')
+    methods: {
+        reset() {
+            this.perspective = 100;
+            this.rotateX = 0;
+            this.rotateY = 0;
+            this.rotateZ = 0;
+        },
+        async copy() {
+            let text = ` transform:${this.box.transform}`;
+            await navigator.clipboard.writeText(text);
 
-      el.setAttribute('readonly', '')
-      el.style.position = 'absolute'
-      el.style.left = '-9999px'
-      el.value = `transform: ${this.box.transform}`
+            alert("Copied!");
 
-      document.body.appendChild(el)
-      el.select()
-      document.execCommand('copy')
-      document.body.removeChild(el)
-    }
-  }
-}).mount('#app')
+            const el = document.createElement("textarea");
+
+            el.setAttribute("readonly", "");
+            el.style.position = "absolute";
+            el.style.left = "-9999px";
+            el.value = `transform: ${this.box.transform}`;
+
+            document.body.appendChild(el);
+            el.select();
+            document.execCommand("copy");
+            document.body.removeChild(el);
+        },
+    },
+}).mount("#app");
