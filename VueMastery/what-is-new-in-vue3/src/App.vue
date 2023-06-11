b4383c8844e9b7d92d89928a0e49978d60e84a6a
Jacek Mateusz Rogowski <rogalowski@gmail.com>
2023-06-11 17:35:06 +0200

    waht-is-new-in-vue3 7:25

---
+++ VueMastery/what-is-new-in-vue3/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+export function useTitle(options) {
+  const { ob = false,  title="dd"} = options
+  const options2 = { ob: true,  title: "ff"} 
+  console.log("ob",ob)
+  console.log("title", title)
+  console.log("options2",options2)
+  console.log("options",options)
+}
+useTitle({ob: null, title: "HAHA"})
+useTitle( "sdkfh")
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    sendData() {
+      this.$emit('sending-start')
+
+
+
+      
+      this.$emit('sending-complete')
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueMastery/what-is-new-in-vue3/src/components/DataSender.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+  <button @click="sendData">Send data</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataSender',
+  emits: [{
+    'sending-start': null,
+    'sending-completed': (payload) => {
+      if (payload.duration & payload.response){
+        return true
+      } else {
+        return false
+      }
+
+    }
+}],
+  // emits: ['sending-start'],
+  methods: {
+    sendData() {
+      this.$emit('sending-start')
+
+ 
+
+      
+      this.$emit('sending-completed', {
+        duration: 2,
+        response: "response  :)"
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
